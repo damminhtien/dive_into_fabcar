544cb88eeb2e239d3b27d0b94ae0354edf671980
damminhtien <damminhtienchl@gmail.com>
2019-05-24 21:11:46 +0700

    done api

---
+++ public/js/scripts/advanced-ui-modals.js
@@ -0,0 +1,10 @@
+/*
+* Modals - Advanced UI
+*/
+$(function() {
+    $('.modal').modal();
+    $('#modal3').modal('open');
+    $('#modal3').modal('close');
+    $('#modal1').modal('open');
+    $('#modal1').modal('close');
+  });
